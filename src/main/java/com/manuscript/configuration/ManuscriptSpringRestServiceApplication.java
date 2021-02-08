7c40933433d11934854aafa10bee98d1ccd12cb7
Neeraj Golda <Neeraj.Golda@global.internal>
2021-02-08 11:23:13 +0530

    Added Custom Exception

---
+++ src/main/java/com/manuscript/configuration/ManuscriptSpringRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manuscript.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuscriptSpringRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManuscriptSpringRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manuscript/configuration/ManuscriptSpringRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuscript.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuscriptSpringRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
